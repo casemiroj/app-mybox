ff0568dce46d4e2862cdf05bd4337826e2280836
casemiro <jv.casemiro@hotmail.com.br>
2021-03-30 10:47:13 -0300

    Adicionando estilização

---
+++ assets/css/index.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const primary = '#231651';
+const darkPrimary = '#1E1345';
+const lightPrimary = '#2D1C66'; 
+const secondary = '#ECA400';
+const black = '#252318'; 
+const white = '#FFFBFF'; 
+const colorAuxliar = '#9DB85F';
+
+const css = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: primary,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+
+export { css }
