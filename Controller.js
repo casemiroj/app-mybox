d5ee6d9761ebbb03a0c979bca01b763af9cd2a88
casemiro <jv.casemiro@hotmail.com.br>
2021-04-23 16:00:08 -0300

    Fazendo CRUD

--- Controller.js
+++ Controller.js
@@ -1,13 +1,46 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const models = require('./models')
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.send('Meu servidor backend já está rodando')
+let user = models.User
+let tracking = models.Tracking
+let product = models.Product
+
+app.get('/create', async (req, res) => {
+    let create = await user.create({ 
+        name: 'João', 
+        password: 'abc',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    })
+    res.send('Usuário criado com sucesso!')
+})
+
+app.get('/read', async (req, res) => {
+    let read = await user.findAll({
+        raw: true
+    })
+    console.log(read)
+})
+
+app.get('/update', async (req, res) => {
+    let update = await user.findByPk(1, 
+        {include:[{all: true}]})
+        .then((response) => {
+            response.Trackings[0].local = 'Nova Cidade'
+            response.Trackings[0].save()
+    })
+})
+
+app.get('/delete', async (req, res) => {
+    user.destroy({
+        where: { id: 2 }
+    })
 })
 
 let port = process.env.PORT || 3000
